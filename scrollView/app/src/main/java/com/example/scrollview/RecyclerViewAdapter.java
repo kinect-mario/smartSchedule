4e121bf72c749569e03b9ce0c2f9092899ebd636
Baldeep <chhabrabaldeepsingh@gmail.com>
2020-06-29 13:09:43 +0530

    error in implementing fragment

--- scrollView/app/src/main/java/com/example/scrollview/RecyclerViewAdapter.java
+++ scrollView/app/src/main/java/com/example/scrollview/RecyclerViewAdapter.java
@@ -1,21 +1,33 @@
 package com.example.scrollview;
 
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
+
+import static com.example.scrollview.R.drawable.bg_blue;
+import static com.example.scrollview.R.drawable.bg_red;
+import static com.example.scrollview.R.drawable.bg_yellow;
+import static com.example.scrollview.R.drawable.blue;
+import static com.example.scrollview.R.drawable.dark_blue;
+import static com.example.scrollview.R.drawable.orange;
+import static com.example.scrollview.R.drawable.red;
 
 /**
  * Created by User on 2/12/2018.
@@ -46,12 +58,28 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public void onBindViewHolder(ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called.");
 
+
         Glide.with(mContext)
                 .asBitmap()
                 .load(mImageUrls.get(position))
                 .into(holder.image);
 
         holder.name.setText(mNames.get(position));
+        switch (position%4) {
+            case 0:
+                holder.constraintLayout.setBackground(mContext.getDrawable(orange));
+                break;
+            case 1:
+                holder.constraintLayout.setBackground(mContext.getDrawable(blue));
+                break;
+            case 2:
+                holder.constraintLayout.setBackground(mContext.getDrawable(dark_blue));
+                break;
+            case 3:
+                holder.constraintLayout.setBackground(mContext.getDrawable(red));
+                break;
+
+        }
 
         holder.image.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,11 +99,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         ImageView image;
         TextView name;
+        ConstraintLayout constraintLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
             image = (ImageView)itemView.findViewById(R.id.image_view);
             name = itemView.findViewById(R.id.name);
+            constraintLayout = itemView.findViewById(R.id.eventLayout);
         }
     }
 }
---
+++ scrollView/app/src/main/java/com/example/scrollview/subjectFragment.java
@@ -0,0 +1,28 @@
+package com.example.scrollview;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class subjectFragment extends Fragment {
+
+    public subjectFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_subject, container, false);
+    }
+}
