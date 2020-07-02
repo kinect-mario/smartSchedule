12311889e3d3c6bc66d6734483b106250d720dd5
anfal <anfalansari1@gmail.com>
2020-07-02 15:51:08 +0530

    added slider to bottomNavigation

--- scrollView/app/src/main/java/com/example/scrollview/MainActivity.java
+++ scrollView/app/src/main/java/com/example/scrollview/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -18,45 +19,78 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    MenuItem prevMenuItem;
+    ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewPager = findViewById(R.id.fragment_container);
         //bottom navigation
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: " + position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
 
 
+            @Override
+            public void onPageScrollStateChanged(int state) {
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new HomeFragment()).commit();
-        }
+            }
+        });
+        setupViewPager(viewPager);
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
                     switch (item.getItemId()) {
                         case R.id.action_home:
-                            selectedFragment = new HomeFragment();
+                            viewPager.setCurrentItem(0);
                             break;
                         case R.id.action_schedule:
-                            selectedFragment = new scheduleFragment();
+                            viewPager.setCurrentItem(1);
                             break;
                         case R.id.action_subject:
-                            selectedFragment = new subjectFragment();
+                            viewPager.setCurrentItem(2);
                             break;
                         case R.id.action_profile:
-                            selectedFragment = new userFragment();
+                            viewPager.setCurrentItem(3);
                     }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
                     return true;
                 }
             };
+
+    //adding fragments to viewpager
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new HomeFragment());
+        viewPagerAdapter.addFragment(new scheduleFragment());
+        viewPagerAdapter.addFragment(new subjectFragment());
+        viewPagerAdapter.addFragment(new userFragment());
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+
 }
