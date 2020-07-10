4884c325cd09bd5c416794fd6cb53046bfeeac4b
anfal <anfalansari1@gmail.com>
2020-07-10 15:30:16 +0530

    compressible image in subject fragment

--- scrollView/app/src/main/java/com/example/scrollview/subjectFragment.java
+++ scrollView/app/src/main/java/com/example/scrollview/subjectFragment.java
@@ -1,5 +1,7 @@
 package com.example.scrollview;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
@@ -11,12 +13,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.scrollview.modal.Subject;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +35,11 @@ public class subjectFragment extends Fragment {
     ConstraintLayout expandableView;
     Button arrowBtn;
     CardView cardView;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private AppBarLayout appBarLayout;
+    private Menu collapsedMenu;
+    private boolean appBarExpanded = true;
+
 
     public subjectFragment() {
         // Required empty public constructor
@@ -47,6 +57,8 @@ public class subjectFragment extends Fragment {
         arrowBtn = view.findViewById(R.id.arrowBtn);
         cardView = view.findViewById(R.id.cardView);
         recyclerView = view.findViewById(R.id.sub_recyclerview);
+        collapsingToolbarLayout = view.findViewById(R.id.collapsing_toolbar);
+        appBarLayout = view.findViewById(R.id.appbar);
 
 
         final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL, false);
@@ -55,13 +67,40 @@ public class subjectFragment extends Fragment {
         List<Subject> subjects = new ArrayList<Subject>();
         for(int i= 0 ; i<6;i++)
         {subjects.add(new Subject());}
-
-
         subjectAdapter subjectAdapter = new subjectAdapter(getContext(),subjects);
         recyclerView.setAdapter(subjectAdapter);
-        if(arrowBtn!=null) {
+        collapsingToolbarLayout.setTitle("Mechanical Engineering");
 
-        }
+       /* Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.subject_illustration);
+         Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @SuppressWarnings("ResourceType")
+            @Override
+            public void onGenerated(Palette palette) {
+                int vibrantColor = palette.getVibrantColor(R.color.primary_500);
+                collapsingToolbar.setContentScrimColor(vibrantColor);
+                collapsingToolbar.setStatusBarScrimColor(R.color.black_trans80);
+            }
+        });*/
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = true;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle("Mechanincal Engineering");
+                    isShow = true;
+                } else if(isShow) {
+                    collapsingToolbarLayout.setTitle(" ");//careful there should a space between double quote otherwise it wont work
+                    isShow = false;
+                }
+            }
+        });
         // Inflate the layout for this fragment
         return view;
     }
